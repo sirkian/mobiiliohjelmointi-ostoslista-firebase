9492ab83fae73df93c594c064db358114f7e2541
sirkian <anssisirkia@gmail.com>
2022-10-15 19:40:12 +0300

    React Native Elements

--- App.js
+++ App.js
@@ -2,14 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { push, ref, onValue, remove } from "firebase/database";
 import { database } from "./firebase.js";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  FlatList,
-  Button,
-} from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { Header, Icon, Input, ListItem, Button } from "react-native-elements";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   const [product, setProduct] = useState("");
@@ -40,88 +35,96 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setProduct(text)}
-        value={product}
-        placeholder="Product"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setAmount(text)}
-        value={amount}
-        placeholder="Amount"
-      />
-      <View style={styles.btnContainer}>
-        <Button title="ADD" onPress={handleSave} />
-      </View>
-      <View style={styles.listContainer}>
-        {items.length > 0 ? (
-          <Text style={styles.text}>Shopping List</Text>
-        ) : (
-          <Text style={styles.textSecondary}>List empty.</Text>
-        )}
-        <FlatList
-          data={items}
-          keyExtractor={(item) => item.key}
-          renderItem={({ item }) => (
-            <View style={styles.list}>
-              <Text>
-                {item.Product}, {item.Amount}
-              </Text>
-              <Text
-                style={styles.textSecondary}
-                onPress={() => handleDelete(item)}
-              >
-                Delete
-              </Text>
-            </View>
-          )}
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header
+          containerStyle={styles.headerContainer}
+          centerComponent={{ text: "SHOPPING LIST", style: styles.headerText }}
         />
+        <View style={styles.inputContainer}>
+          <Input
+            style={styles.input}
+            onChangeText={(text) => setProduct(text)}
+            value={product}
+            label="Product"
+          />
+          <Input
+            style={styles.input}
+            onChangeText={(text) => setAmount(text)}
+            value={amount}
+            label="Amount"
+          />
+        </View>
+        <View style={styles.btnContainer}>
+          <Button
+            type="clear"
+            title="Add"
+            icon={<Icon name="push-pin" size={24} color="#424463" />}
+            titleStyle={{ color: "#424463" }}
+            onPress={handleSave}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.key}
+            renderItem={({ item }) => (
+              <ListItem topDivider>
+                <ListItem.Content style={styles.list}>
+                  <View>
+                    <ListItem.Title>{item.Product}</ListItem.Title>
+                    <ListItem.Subtitle>{item.Amount}</ListItem.Subtitle>
+                  </View>
+                  <Icon
+                    name="delete-outline"
+                    onPress={() => handleDelete(item)}
+                  ></Icon>
+                </ListItem.Content>
+              </ListItem>
+            )}
+          />
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#FFF",
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 150,
+  },
+  headerContainer: {
+    backgroundColor: "#424463",
+    paddingTop: 0,
+    paddingBottom: 20,
+  },
+  headerText: {
+    fontSize: 16,
+    color: "#FFF",
+    fontWeight: "bold",
+    letterSpacing: 1.5,
+  },
+  inputContainer: {
+    width: "50%",
+    marginTop: 20,
   },
   input: {
-    borderWidth: 1,
-    borderColor: "gray",
-    width: 200,
-    height: 40,
-    borderRadius: 5,
-    textAlign: "center",
-    marginBottom: 7,
+    color: "#424463",
   },
   btnContainer: {
-    flexDirection: "row",
-    margin: 10,
-  },
-  text: {
-    fontSize: 18,
-    margin: 8,
-  },
-  textSecondary: {
-    color: "darkgrey",
-    marginLeft: 5,
+    marginTop: 5,
+    marginBottom: 20,
   },
   listContainer: {
+    width: "90%",
     display: "flex",
-    alignItems: "center",
-    width: "100%",
-    padding: 3,
+    maxHeight: 250,
   },
   list: {
     flexDirection: "row",
-    justifyContent: "center",
-    marginBottom: 6,
+    justifyContent: "space-between",
   },
 });
